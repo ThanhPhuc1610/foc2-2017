fd95855c857a325e3189122128dea13f1f1cf93c
US <US@A1M25>
2017-08-05 14:09:30 +0700

    Chuc nang nhan new mo mot cua so moi

--- D1-T133747-Phuc/src/MainWindow.java
+++ D1-T133747-Phuc/src/MainWindow.java
@@ -1,13 +1,20 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MainWindow extends JFrame {
 
-	private JPanel contentPane;
+	private JDesktopPane contentPane;
 
 	/**
 	 * Launch the application.
@@ -31,11 +38,43 @@ public class MainWindow extends JFrame {
 	public MainWindow() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnAdd = new JMenu("Add");
+		menuBar.add(mnAdd);
+		
+		JMenuItem mntmNew = new JMenuItem("New");
+		
+		mnAdd.add(mntmNew);
+		
+		JMenuItem mntmOpen = new JMenuItem("Open");
+		mnAdd.add(mntmOpen);
+		
+		JMenuItem mntmSave = new JMenuItem("Save");
+		mnAdd.add(mntmSave);
+		
+		JMenu mnEdit = new JMenu("Edit");
+		menuBar.add(mnEdit);
+		
+		JMenu mnDelete = new JMenu("Delete");
+		menuBar.add(mnDelete);
+		contentPane = new JDesktopPane();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.setLayout(null);
 		setContentPane(contentPane);
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
+		
+		//xu ly su kien
+		mntmNew.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				JInternalFrame newWindow = new JInternalFrame("New Window 1", true, true, true);
+				newWindow.setVisible(true);
+				newWindow.setSize(300, 300);
+				contentPane.add(newWindow);
+			}
+		});
 	}
 
 }
